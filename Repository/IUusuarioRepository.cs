d27468cc47c0636bcd35fed84975679cda833fc2
emerson143 <emerson.143@hotmail.com>
2022-11-16 20:06:43 -0300

    criando irteface

---
+++ Repository/IUusuarioRepository.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.net_api_crud.Repository;
+
+public interface IUusuarioRepository
+{
+    
+}
